e901af01969d98490526b35a4650089eb25027c0
Sannidhya Chauhan <sannidhya@google.com>
2026-06-22 03:54:10 -0700

    Enable continuous profiling for HostTracer
    PiperOrigin-RevId: 935970674

--- third_party/tsl/tsl/profiler/lib/continuous_profiler_orchestrator.h
+++ third_party/tsl/tsl/profiler/lib/continuous_profiler_orchestrator.h
@@ -74,6 +74,13 @@ class ContinuousProfilerOrchestrator : public ProfilerInterface {
   // Stops background thread and profiling.
   absl::Status Stop() override {
     StopIngestionThread();
+    auto result = profiler_->Consume();
+    if (result.ok()) {
+      absl::MutexLock lock(&mutex_);
+      circular_buffer_.push_back(std::move(result->data));
+    } else if (!absl::IsUnimplemented(result.status())) {
+      LOG(WARNING) << "Final Consume failed during Stop: " << result.status();
+    }
     return profiler_->Stop();
   }
 
@@ -102,7 +109,6 @@ class ContinuousProfilerOrchestrator : public ProfilerInterface {
   ProfilerType* profiler() { return profiler_.get(); }
   const ProfilerType* profiler() const { return profiler_.get(); }
 
- private:
   std::vector<std::any> PopBuffer() {
     absl::MutexLock lock(mutex_);
     std::vector<std::any> chunks;
@@ -113,7 +119,10 @@ class ContinuousProfilerOrchestrator : public ProfilerInterface {
     circular_buffer_.clear();
     return chunks;
   }
+
+ private:
   void IngestionLoop() {
+    LOG(INFO) << "ContinuousProfilerOrchestrator::IngestionLoop started";
     while (true) {
       absl::StatusOr<ConsumeResult> result = profiler_->Consume();
 
